3977c868469c2de30e4c20d26d24bca44f6d57cf
Svidinskiy <100801664+Svidinskiy@users.noreply.github.com>
2023-04-16 20:33:15 +0400

    Сделано ДЗ 2.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Bicycle bicycle = new Bicycle("Велосипед 1", 2);
+        Bicycle bicycle2 = new Bicycle("Велосипед 2", 2);
+
+        Car car = new Car("Машина 1", 4);
+        Car car2 = new Car("Машина 2", 4);
+
+        Truck truck = new Truck("Грузовик 1", 6);
+        Truck truck2 = new Truck("Грузовик 2", 8);
+
+        ServiceStation station = new ServiceStation();
+
+        station.check(bicycle);
+        System.out.println("\n");
+
+        station.check(bicycle2);
+        System.out.println("\n");
+
+        station.check(car);
+        System.out.println("\n");
+
+        station.check(car2);
+        System.out.println("\n");
+
+        station.check(truck);
+        System.out.println("\n");
+
+        station.check(truck2);
     }
 }
+
